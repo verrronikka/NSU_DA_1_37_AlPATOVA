a9d310798483294aa5af9c846437dc75c72b2f01
verrronikka <nikaalpatova130306@gmail.com>
2025-09-24 19:19:20 +0700

    Update moving_average.py

--- moving_average.py
+++ moving_average.py
@@ -4,16 +4,16 @@ import os
 import argparse
 from pathlib import Path
 
-def create_time_series(file_path):
+def create_time_series(file_path, date_col, value_col):
     """
     Читает временной ряд из файла csv
     
     Args:
         file_path (str): путь к файлу с данными
-        
+        date_col (str): название столбца с датами
+        value_col (str): название столбца со значениями
     Returns:
         pd.DataFrame: DataFrame с колонками 'date' и 'value'
-        
     Raises:
         FileNotFoundError: если файл не существует
         ValueError: если не поддерживается формат файла
@@ -27,54 +27,65 @@ def create_time_series(file_path):
 
     try: 
         if file_ext == '.csv':
-            df = pd.read_csv(file_path, parse_dates=['date'])
+            df = pd.read_csv(file_path, parse_dates=[date_col])
         else:
             raise ValueError(f"Неподдерживаемый формат файла: {file_ext}")
         
-        if 'date' not in df.columns:
-            raise KeyError("Колонка 'date' не найдена в файле")
-        if 'value' not in df.columns:
-            raise KeyError("Колонка 'value' не найдена в файле")
+        if date_col not in df.columns:
+            raise KeyError(f"Колонка {date_col}  не найдена в файле")
+        if value_col not in df.columns:
+            raise KeyError(f"Колонка {value_col} не найдена в файле")
+        df[date_col] = pd.to_datetime(df[date_col])
 
         return df
     except Exception as e:
         raise Exception(f"Ошибка чтения файла: {str(e)}")
 
 
-def calculation_moving_average(df, window):
+def calculation_moving_average(df, window, value_col, new_col=None):
     """
     Считает скользящее среднее
     
     Args:
-        df (pd.DataFrame): DataFrame с колонками 'date' и 'value' 
-        window (int): размер окна для расчета скользящего среднего
+        df (pd.DataFrame): DataFrame
+        window (int): размер окна
+        value_col (str): название столбца с значениями
+        new_col (str): название для нового столбца
     Returns:
-        pd.DataFrame: DataFrame с колонками 'date', 'value', 'SMA'  
+        pd.DataFrame: DataFrame с новой колонкой
+    Raises:
+        TypeError: если значения не числовые
     """
+    try:
+        numeric_values = pd.to_numeric(df[value_col], errors='raise')
+    except ValueError as e:
+        raise TypeError(f"Столбец 'value' содержит нечисловые значения: {e}")
 
-    df['SMA'] = df['value'].rolling(window=window, min_periods=1).mean()
+    df[new_col] = df[value_col].rolling(window=window, min_periods=1).mean()
 
     return df
 
-def build_graphic(df, output_file, format_file):
+def build_graphic(df, output_file, format_file, date_col, value_col, new_col):
     """
     Строит и сохраняет график временного ряда и скользящего среднего (в формате jpg, pdf, png с названием moving_average)
     
     Args:
-        df (pd.DataFrame): DataFrame с колонками 'date', 'value', 'SMA'
-        output_dir (str): директория для сохранения файла
-        format_file (str): формат файла ('png', 'pdf', 'jpg')
-
+        df (pd.DataFrame): DataFrame с данными
+        output_file (str): директория для сохранения файла
+        format_file (str): формат файла
+        date_col (str): название столбца с датами
+        value_col (str): название столбца с исходными значениями
+        new_col (str): название нового столбца
     Returns:
         plt.Figure: объект фигуры matplotlib
     """
     fig = plt.figure(figsize=(12,6))
-    plt.plot(df['date'], df['value'], label='Исходные данные', linewidth=2)
-    plt.plot(df['date'], df['SMA'], label='Скользящее среднее', linewidth=2)
+    plt.plot(df[date_col], df[value_col], label='Исходные данные', linewidth=2)
+    plt.plot(df[date_col], df[new_col], label=new_col, linewidth=2)
 
-    plt.title('Скользящее среднее')
-    plt.xlabel('Дата')
-    plt.ylabel('Значение')
+    plt.title('Временной ряд и скользящее среднее')
+    plt.xlabel(date_col)
+    plt.ylabel(value_col)
     plt.legend()
     plt.grid(True, alpha=0.3)
     plt.xticks(rotation=45)
@@ -100,18 +111,19 @@ def main():
     parser.add_argument('-o', '--output', default='output', help='Директория для результатов')
     parser.add_argument('-w', '--window', type=int, default=3, help='Размер окна для скользящего среднего (по умолчанию: 3)')
     parser.add_argument('--format', default='png', choices=['png', 'pdf', 'jpg'], help='Формат графика')
+    parser.add_argument('--date-col', default='date', help='Название столбца с датами (по умолчанию: date)')
+    parser.add_argument('--value-col', default='value', help='Название столбца с числовыми значениями (по умолчанию: value)')
+    parser.add_argument('--new-col', help='Название для нового столбца')
     args = parser.parse_args()
 
     print(args.output)
-    df = create_time_series(args.input_file)
+    df = create_time_series(args.input_file, args.date_col, args.value_col)
     #  вычисление скользящего среднего
-    res = calculation_moving_average(df, args.window)
-    #  построение графика
-    fig = build_graphic(res, args.output, args.format)
+    res = calculation_moving_average(df, args.window, args.value_col, args.new_col)
+    #  построение и сохранение графика
+    fig = build_graphic(res, args.output, args.format, args.date_col, args.value_col, args.new_col)
     
-    return 0
 
 if __name__ == "__main__":
     main()
-
 
